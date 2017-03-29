d71f0fecca2c0037b19203e86acaf52e3d56192b
alexey-savin <alexeyv.savin@gmail.com>
2017-03-29 21:40:35 +0300

    logic

---
+++ ThreadSafeAccount/ThreadSafeAccount/Account.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadSafeAccount
+{
+    class Account
+    {
+        private object _locker = new object();
+
+        private int _balance = 0;
+        private Random _rand = new Random();
+
+        public Account(int initial)
+        {
+            _balance = initial;
+        }
+
+        private int Withdraw(int amount)
+        {
+            if (_balance < 0)
+            {
+                throw new InvalidOperationException("Negative balance detected");
+            }
+
+            lock (_locker)
+            {
+                if (_balance >= amount)
+                {
+                    Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}");
+
+                    Console.WriteLine($"Balance before Withdraw {_balance}");
+                    Console.WriteLine($"Amount to Withdraw -{amount}");
+
+                    _balance = _balance - amount;
+
+                    Console.WriteLine($"Balance after Withdraw {_balance}");
+
+                    return amount;
+                }
+                else
+                {
+                    return 0; // transaction rejected
+                }
+            }
+        }
+
+        public void DoTransactions()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Withdraw(_rand.Next(1, 100));
+            }
+
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} exited");
+        }
+    }
+}
---
+++ ThreadSafeAccount/ThreadSafeAccount/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadSafeAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc = new Account(1000);
+
+            int threadsCount = 2;
+            Thread[] threads = new Thread[threadsCount];
+
+            for (int i = 0; i < threadsCount; i++)
+            {
+                Thread t = new Thread(new ThreadStart(acc.DoTransactions));
+                threads[i] = t;
+            }
+
+            for (int i = 0; i < threadsCount; i++)
+            {
+                threads[i].Start();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
